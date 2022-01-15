2f9f05e68b1033e16f9882f97d2f43f92a0fd8ef
RosaBri <gholasoy532@gmail.com>
2022-01-15 13:23:42 -0600

    Aumentando explicacion

--- data/ejerercicio_1104.sh
+++ data/ejerercicio_1104.sh
@@ -1,7 +1,7 @@
-##Saavedra and Stouffer (2013) studied several plant–pollinator networks.
 These can be represented as rectangular matrices where the rows are polli-
-nators, the columns plants, a 0 indicates the absence and 1 the presence of an
+nators, the columns plants, a 0 indicates the absence and 1 the presence of a
 Interaction between the plant and the pollinator.
+
 - Intrucciones de comandos
 -Usaremos el archivo n3.txt
 # n de columnas
@@ -21,9 +21,3 @@ cat ../Saavedra2013/n3.txt | wc -l
 " n de columnas "
 
 head -n1 ../Saavedra2013/n3.txt | tr -d " " | tr -d "\ n" | wc -c
-
-## Write a script that prints the numbers of rows and columns for each network
-### Use of For
--for permite repetir una tarea con ligeras variaciones, generalmente si son tareas
-
-for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done
