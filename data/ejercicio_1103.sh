5fae7c8166e9486ce388da6b620219add8955ee5
AndersonYumbo <95886441+Slayer098@users.noreply.github.com>
2022-01-15 13:39:46 -0600

    Update ejercicio_1103.sh
    Correccion

--- data/ejercicio_1103.sh
+++ data/ejercicio_1103.sh
@@ -7,30 +7,30 @@ Interaction between the plant and the pollinator.
 ## Codigos
 Imprimi el n3.txt usando cat
 - Intrucciones de comandos
--Usaremos el archivo n3.txt 
+- Usaremos el archivo n3.txt 
 # n de columnas
--imprimo cat../Saavedra2013/n3.txt | wc -l 
--cat imprime contenido de los archivos ( cat file1 file2 file3)
--wc -l Cuenta solo líneas ( sort w, sort c) 
+- imprimo cat../Saavedra2013/n3.txt | wc -l 
+- cat imprime contenido de los archivos ( cat file1 file2 file3)
+- wc -l Cuenta solo líneas ( sort w, sort c) 
 #n de filas 
--head muestra las filas(lo que pongamos) 
--tr translasion de caracteres de un archivo 
+- head muestra las filas(lo que pongamos) 
+- tr translasion de caracteres de un archivo 
 
 cat n3.txt
 " n de filas"
 
-cat ../Saavedra2013/n3.txt | wc -l
+cat ./Saavedra2013/n3.txt | wc -l
 
 
 " n de columnas "
 
-head -n1 ../Saavedra2013/n3.txt | tr -d " " | tr -d "\ n" | wc -c
+head -n1 ./Saavedra2013/n3.txt | tr -d " " | tr -d "\ n" | wc -c
 
 
 ## Write a script that prints the numbers of rows and columns for each network 
 ### Use of For 
 
--for permite repetir una tarea con ligeras variaciones, generalmente si son archivos
+- for permite repetir una tarea con ligeras variaciones, generalmente si son archivos
 
 for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done
 
